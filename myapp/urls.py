69f29693d72a50c6df2d11924c9bcdd9bbdace93
TOEYTANAPA <akanip2@gmail.com>
2018-01-24 00:19:37 +0700

    noti

--- myapp/urls.py
+++ myapp/urls.py
@@ -11,8 +11,9 @@ urlpatterns = [
     url(r'^employer-search/$',views.employer_search, name="employer_search"),
     url(r'^create-job/$',views.create_job, name="create_job"),
     url(r'^job/(?P<job_name>.*)/(?P<job_id>\d+)$',views.job_detail, name="job_detail"),
+    url(r'^notifications/$',views.notification_mobile, name="noti"),
     # url(r'^contactus/$',views.contact, name="contactus"),
-    
+
 
     # url(r'^',  include('myapp.urls',namespace='mainapp')),
     # url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
